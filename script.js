ab63c52cda6ddc31ef0d21919624b4187f5ffa20
santandanao <131091831+santandanao@users.noreply.github.com>
2023-06-24 17:57:06 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+// ゲームオブジェクトの初期化
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height - 30,
+    dx: 2,
+    dy: -2,
+    radius: 10
+};
+
+const paddle = {
+    width: 75,
+    height: 10,
+    x: (canvas.width - 75) / 2
+};
+
+// ボールの描画
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// パドルの描画
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// ゲームの更新と描画
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBall();
+    drawPaddle();
+
+    // ボールの移動
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    // 壁との衝突判定
+    if (ball.x < ball.radius || ball.x > canvas.width - ball.radius) {
+        ball.dx = -ball.dx;
+    }
+    if (ball.y < ball.radius || ball.y > canvas.height - ball.radius) {
+        ball.dy = -ball.dy;
+    }
+
+    // ここからゲームのロジックを実装
+}
+
+// ゲームループ
+setInterval(update, 10);
