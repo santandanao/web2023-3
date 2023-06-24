41d64f4b2484a1c4bc33b6934f8d6d87a0e9ae41
santandanao <131091831+santandanao@users.noreply.github.com>
2023-06-24 17:59:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,40 @@ function update() {
 
 // ゲームループ
 setInterval(update, 10);
+
+// パドルの操作
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+    }
+});
+
+function movePaddle() {
+    if (rightPressed && paddle.x < canvas.width - paddle.width) {
+        paddle.x += 7;
+    } else if (leftPressed && paddle.x > 0) {
+        paddle.x -= 7;
+    }
+}
+
+// update関数内にmovePaddle()を追加
+function update() {
+    // 省略...
+    
+    movePaddle();
+
+    // 省略...
+}
